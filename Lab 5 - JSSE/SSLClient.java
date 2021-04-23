202fd97f0a392520800155d6a99c2bccd0ad0624
MechJM <matosjoaops@protonmail.com>
2021-04-23 13:03:23 +0100

    Doing lab 5

--- Lab 5 - JSSE/SSLClient.java
+++ Lab 5 - JSSE/SSLClient.java
@@ -1,5 +1,107 @@
-public class SSLClient {
-    public static void main(String[] args) {
-        int port = Integer.parseInt(args[0]);
-    }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
+public class SSLClient
+{
+    public static void main(String[] args) throws IOException
+    {
+        String response = new String();
+        String message = new String();
+        try
+        {
+            //process command line args
+            String host = args[0];
+            int port = Integer.parseInt(args[1]);
+            String operation = args[2];
+            List<String> operands = new ArrayList<>();
+
+            InetAddress address = InetAddress.getByName(host);
+            SSLSocket socket = (SSLSocket) SSLSocketFactory.getDefault().createSocket(address, port);
+        
+            if (operation.equals("REGISTER"))
+            {
+                operands.add(args[3]);
+                operands.add(args[4]);
+
+                if (args.length > 5)
+                {
+                    String[] cipherSuites = new String[args.length - 5];
+                    for (int i = 5; i < args.length; i++) {
+                        cipherSuites[i - 5] = args[i];
+                    }
+                    socket.setEnabledCipherSuites(cipherSuites);
+                }
+            }
+            else
+            {
+                operands.add(args[3]);
+                if (args.length > 4)
+                {
+                    String[] cipherSuites = new String[args.length - 4];
+                    for (int i = 4; i < args.length; i++) {
+                        cipherSuites[i - 4] = args[i];
+                    }
+                    socket.setEnabledCipherSuites(cipherSuites);
+                }
+            } 
+
+            //build message
+            message = operation;
+            for (String operand : operands)
+            {
+                message += " " + operand;
+            }
+
+            
+            
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            //send message
+            out.println(message);
+
+            //process response
+            response = in.readLine();
+            //close socket
+            socket.shutdownOutput();
+            socket.close();
+        }
+        catch (SocketException e)
+        {
+             e.printStackTrace();
+            System.out.println("Couldn't setup socket!");
+            response = "ERROR";
+        }
+        catch (UnknownHostException e)
+        {
+            // e.printStackTrace();
+            System.out.println("Couldn't get host!");
+            response = "ERROR";
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.toString());
+            System.out.println("Couldn't read/write to/from socket!");
+            response = "ERROR";
+        }
+        finally
+        {
+            //print log
+            System.out.println("SSLClient: " + message + " : " + response);
+        }
+
+    }    
 }
--- Lab 5 - JSSE/SSLServer.java
+++ Lab 5 - JSSE/SSLServer.java
@@ -1,5 +1,104 @@
-public class SSLServer {
-    public static void main(String[] args) {
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+
+public class SSLServer 
+{
+    public static void main(String[] args)
+    {
+        //processing command line arg and setting up
         int port = Integer.parseInt(args[0]);
-    }
+        Map<String,String> table = new HashMap<>();
+        String response = new String();
+
+        try
+        {
+            SSLServerSocket serverSocket = (SSLServerSocket) SSLServerSocketFactory.getDefault().createServerSocket(port);
+            if (args.length > 1)
+            {
+                String[] cipherSuites = new String[args.length - 1];
+                for (int i = 1; i < args.length; i++)
+                {
+                    cipherSuites[i - 1] = args[i];
+                }
+                serverSocket.setEnabledCipherSuites(cipherSuites);
+            }
+           
+            while (true)
+            {   
+                SSLSocket clientSocket = (SSLSocket) serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                String request = in.readLine();
+                String[] requestElems = request.split(" ");
+                
+                if (requestElems[0].equals("REGISTER"))
+                {
+                    String dnsName = requestElems[1];
+                    String ipAddress = requestElems[2];
+                    String existingEntry = table.get(dnsName);
+                    if (existingEntry == null)
+                    {
+                        table.put(dnsName, ipAddress);
+                        response = String.valueOf(table.size());
+                    }
+                    else response = "-1";
+                }
+                else if (requestElems[0].equals("LOOKUP"))
+                {
+                    String dnsName = requestElems[1];
+                    String existingEntry = table.get(dnsName);
+                    if (existingEntry == null) response = "NOT_FOUND";
+                    else response = dnsName + " " + existingEntry;
+                }
+
+                out.println(response);
+
+                //print log
+                String log = new String("SSLServer:");
+                for (String elem : requestElems)
+                {
+                    log += " " + elem;
+                }
+                System.out.println(log);
+
+                clientSocket.shutdownInput();
+                clientSocket.shutdownOutput();
+                clientSocket.close();
+            }
+
+        }
+        catch (SocketException e)
+        {
+            // e.printStackTrace();
+            System.out.println("Failed to open socket!");
+        }
+        catch (SocketTimeoutException e)
+        {
+            System.out.println("Waited for too long!");
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.toString());
+            System.out.println("Failed to read/write to/from socket!");
+        }
+
+    }    
 }
